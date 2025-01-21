34db9d327564faeda7aaa15ba61b5fe54fb6869a
rrumana <ryan.rumana@gmail.com>
2025-01-21 13:48:18 -0800

    Added some new plugins

--- lua/setup/lazy/harpoon.lua
+++ lua/setup/lazy/harpoon.lua
@@ -8,7 +8,7 @@ return {
         -- REQUIRED
         harpoon:setup()
         -- REQUIRED    end
-        
+
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
